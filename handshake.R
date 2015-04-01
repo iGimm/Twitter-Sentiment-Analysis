4e440166eceaac9af5e643aad4ca787fcfa044e4
iGimm <imr.gimm@gmail.com>
2015-04-01 16:27:46 -0600

    Setting up the conection between Twitter and RStudio

---
+++ handshake.R
@@ -0,0 +1,40 @@
+install.packages(c("twitteR","ROAuth","plyr","stringr","ggplot2"),dependencies=T)
+
+# Authenticating with twitter API
+library(twitteR)
+library(ROAuth)
+library(plyr)
+library(stringr)
+library(ggplot2)
+
+# Accessing the twitter API
+requestURL <- "https://api.twitter.com/oauth/request_token"
+accessURL  =  "http://api.twitter.com/oauth/access_token"
+authURL    =  "http://api.twitter.com/oauth/authorize"
+
+consumerKey    = "L1xbk85CKbvtZpuI6DC9L3c3U"
+consumerSecret = "tjmq76hWIgpw7ZaYaMOzVpFq7RRxcfpWpf11ZFZCybYbth5LLx"
+Cred <- OAuthFactory$new(consumerKey    = consumerKey,
+                         consumerSecret = consumerSecret,
+                         requestURL     = requestURL,
+                         accessURL      = accessURL,
+                         authURL        = authURL)
+Cred$handshake(cainfo = system.file("CurlSSL","cacert.pem",package = "RCurl"))
+
+# Save these credentials and register
+save(Cred, file="twitter authentication.Rdata")
+load("twitter authentication.Rdata")
+registerTwitterOAuth(Cred)
+
+
+# As SSL is required I'll proceed with the next way of conection between apps
+api_key             = ""
+api_secret          = ""
+access_token        = ""
+access_token_secret = ""
+setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
+
+
+#getting tweets from Twitter
+iPhone.list = searchTwitter('#iphone', n=100, cainfo="cacert.pem")
