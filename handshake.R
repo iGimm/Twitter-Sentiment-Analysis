a3e6d137efd2d7006653fe3fc4730ed13b3b2420
iGimm <imr.gimm@gmail.com>
2015-04-01 22:22:27 -0600

    the result of the search is now exported to a csv file

--- handshake.R
+++ handshake.R
@@ -28,13 +28,19 @@ registerTwitterOAuth(Cred)
 
 
 # As SSL is required I'll proceed with the next way of conection between apps
-api_key             = ""
-api_secret          = ""
-access_token        = ""
-access_token_secret = ""
+#api_key             = ""
+#api_secret          = ""
+#access_token        = ""
+#access_token_secret = ""
+#setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
+
+api_key             = "L1xbk85CKbvtZpuI6DC9L3c3U"
+api_secret          = "tjmq76hWIgpw7ZaYaMOzVpFq7RRxcfpWpf11ZFZCybYbth5LLx"
+access_token        = "158590874-WG6Bo3wi09Tzj6yZWxo8QtTm4C4FeLNhujndXjTh"
+access_token_secret = "YvV3KsomGL8QmqaqkoVlLsjBYS0AzgbQ03EJZbcPRNYE5"
 setup_twitter_oauth(api_key,api_secret,access_token,access_token_secret)
 
-
-
 #getting tweets from Twitter
-iPhone.list = searchTwitter('#iphone', n=100, cainfo="cacert.pem")
+iPhone.list = searchTwitter('#Apple', n=100, cainfo="cacert.pem")
+iPhone.list = searchTwitter('#Apple', n=5)
+iPhone.list
